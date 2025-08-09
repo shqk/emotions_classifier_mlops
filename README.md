9042e88c357406a5b5753340e07fc58f93bb7044
Nathan <63520059+shqk@users.noreply.github.com>
2025-08-09 20:25:27 +0200

    configuration gcp terminée

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ uvicorn src.main:app --reload
 ou
 
 fastapi dev src/main.py
+
+build pour cloud run:
+docker buildx build --platform linux/amd64 ...
--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,43 @@ output "project_id" {
   description = "ID du projet GCP"
   value       = var.project_id
 }
+
+resource "google_cloud_run_service" "emotions_api" {
+  location = var.region
+  name     = "emotions-api"
+
+  template {
+    spec {
+      containers {
+        image = "eu.gcr.io/movie-predict-467214-k5/emotions-api:v1"
+
+        ports {
+          container_port = 80
+        }
+
+        resources {
+          limits = {
+            cpu = "1000m"
+            memory = "2Gi"
+          }
+        }
+      }
+    }
+  }
+  traffic {
+    percent = 100
+    latest_revision = true
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "public_access" {
+  member  = "allUsers"
+  role    = "roles/run.invoker"
+  service = google_cloud_run_service.emotions_api.name
+  location = google_cloud_run_service.emotions_api.location
+}
+
+output "api_url" {
+  description = "URL publique de l'API"
+  value = google_cloud_run_service.emotions_api.status[0].url
+}
